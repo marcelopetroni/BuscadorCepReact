8c5fa0d7f863cc3946be09d7dfdf1283289dc9c6
Marcelo Petroni <marceloapetroni@hotmail.com>
2023-10-22 15:34:26 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./BuscadorCepReact",
+  base: "https://github.com/marcelopetroni/BuscadorCepReact",
 })
 server: {
   host: true
