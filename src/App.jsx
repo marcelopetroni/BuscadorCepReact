6e7ca24be031b772a4c1848ca298a118143e7adf
Marcelo Petroni <marceloapetroni@hotmail.com>
2023-10-24 19:02:38 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './styles.css';
 import { FiSearch } from 'react-icons/fi';
 import api from './services/api';
